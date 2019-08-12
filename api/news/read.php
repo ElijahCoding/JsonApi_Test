89dbbff63a57dc6b827af734b0fabb377d896357
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-12 13:12:05 +0300

    news read with participant

---
+++ api/news/read.php
@@ -0,0 +1,47 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+include_once '../../config/Database.php';
+include_once '../../models/News.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$news = new News($db);
+
+$result = $news->read();
+$num = $result->rowCount();
+
+if ($num > 0) {
+    $news_arr = array();
+
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+
+        $news_item = array(
+            'ID' => $ID,
+            'NewsTitle' => $NewsTitle,
+            'NewsMessage' => $NewsMessage,
+            'LikesCounter' => $LikesCounter,
+            'Participant' => [
+                'Email' => $Email,
+                'Name' => $Name
+            ]
+        );
+
+        array_push($news_arr, $news_item);
+    }
+
+    $data = [
+        'status' => 'ok',
+        'payload' => $news_arr
+    ];
+
+    echo json_encode($data);
+} else {
+    echo json_encode(
+        array('message' => 'No Speakers Found')
+    );
+}
--- models/News.php
+++ models/News.php
@@ -18,7 +18,15 @@ class News
 
     public function read()
     {
+        $query = "SELECT n.ID, n.NewsTitle, n.NewsMessage, n.LikesCounter, p.ID, p.Email, p.Name
+                  FROM {$this->table} n
+                  LEFT JOIN Participant p
+                  ON n.ParticipantId = p.ID";
 
+        $statement = $this->conn->prepare($query);
+        $statement->execute();
+
+        return $statement;
     }
 
     public function read_single()
